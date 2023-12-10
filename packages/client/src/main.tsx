fff3533c5766afa99f47c717f5fa8d3299306bc6
Pavlov Sergey <pavlov_sv3@nlmk.com>
2023-12-10 16:29:29 +0300

    feat(REC-26): Добавил компонент EB

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { ErrorBoundary, RootBoundary } from './components/RootBoundary'
+import { RootBoundary } from './components/RootBoundary'
 
 const router = createBrowserRouter([
   {
