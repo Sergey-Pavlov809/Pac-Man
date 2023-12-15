783680d36399bf34ff4ad83fd08cc4ed26fd6e32
Pavlov Sergey <pavlov_sv3@nlmk.com>
2023-12-15 14:56:08 +0300

    fix linter

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -4,13 +4,13 @@ import { Navigate } from 'react-router-dom'
 
 function App(): JSX.Element {
   useEffect(() => {
-    //const fetchServerData = async (): Promise<void> => {
-    //const url = `http://localhost:${__SERVER_PORT__}`
-    //const response = await fetch(url)
-    //const data = await response.json()
-    //  console.log(data)
-    //}
-    //fetchServerData()
+    const fetchServerData = async (): Promise<void> => {
+      const url = `http://localhost:${__SERVER_PORT__}`
+      const response = await fetch(url)
+      const data = await response.json()
+      console.log(data)
+    }
+    fetchServerData()
   }, [])
   return <Navigate to="/sign-in" replace={true} />
 }
