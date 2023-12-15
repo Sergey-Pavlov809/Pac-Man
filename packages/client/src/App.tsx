c77463b6efc686a6966d8e348488fef27fc06949
Pavlov Sergey <pavlov_sv3@nlmk.com>
2023-12-15 14:57:23 +0300

    fix lint

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,18 +1,17 @@
 import { useEffect } from 'react'
 import './App.css'
-import { Navigate } from 'react-router-dom'
 
 function App(): JSX.Element {
   useEffect(() => {
-    const fetchServerData = async (): Promise<void> => {
-      const url = `http://localhost:${__SERVER_PORT__}`
-      const response = await fetch(url)
-      const data = await response.json()
-      console.log(data)
-    }
-    fetchServerData()
+    //const fetchServerData = async (): Promise<void> => {
+    //const url = `http://localhost:${__SERVER_PORT__}`
+    //const response = await fetch(url)
+    //const data = await response.json()
+    //  console.log(data)
+    //}
+    //fetchServerData()
   }, [])
-  return <Navigate to="/sign-in" replace={true} />
+  return <div className="App">Вот тут будет жить ваше приложение :)</div>
 }
 
 export default App
