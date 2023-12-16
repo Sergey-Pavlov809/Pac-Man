af58c250501790a4917ef943d35cd77f25f91a9a
Pavlov Sergey <pavlov_sv3@nlmk.com>
2023-12-16 15:13:10 +0300

    fix

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -3,13 +3,13 @@ import './App.css'
 
 function App(): JSX.Element {
   useEffect(() => {
-    //const fetchServerData = async (): Promise<void> => {
-    //const url = `http://localhost:${__SERVER_PORT__}`
-    //const response = await fetch(url)
-    //const data = await response.json()
-    //  console.log(data)
-    //}
-    //fetchServerData()
+    const fetchServerData = async (): Promise<void> => {
+      const url = `http://localhost:${__SERVER_PORT__}`
+      const response = await fetch(url)
+      const data = await response.json()
+      console.log(data)
+    }
+    fetchServerData()
   }, [])
   return <div className="App">Вот тут будет жить ваше приложение :)</div>
 }
