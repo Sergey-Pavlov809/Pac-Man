9ba9f939aea7615373ff7cb0d8cdf7232ef5c608
Pavlov Sergey <pavlov_sv3@nlmk.com>
2023-12-28 15:04:29 +0300

    add

--- packages/client/src/hooks/useAppDispatch/index.ts
+++ packages/client/src/hooks/useAppDispatch/index.ts
@@ -1,5 +1,6 @@
-import { useDispatch } from 'react-redux'
-import { AppDispatch } from '../../store/types'
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
+import { AppDispatch, RootState } from '../../store/types'
 
-export const useAppDispatch = (): AppDispatch => useDispatch<AppDispatch>
+export const useAppDispatch = (): AppDispatch => useDispatch<AppDispatch>()
+
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
